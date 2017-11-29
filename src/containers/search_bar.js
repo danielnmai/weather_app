ed919af83a29c7c0709302a85af9cba9a0a2df42
Daniel Mai <danielmai1728@gmail.com>
2017-11-28 22:14:05 -0800

    add preventDefault action to form

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,35 +1,32 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 export default class SearchBar extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { term: ''};
+    this.state = {
+      term: ''
+    };
     this.onInputChange = this.onInputChange.bind(this);
   }
 
   onInputChange(event) {
-    this.setState({ term: event.target.value });
+    this.setState({term: event.target.value});
   }
 
   //Change something
-  onFormSubmit(event){
-    // event.preventDefault();
+  onFormSubmit(event) {
+    event.preventDefault();
   }
 
   render() {
-    return (
-      <form onSubmit={this.onFormSubmit} className="input-group">
-        <input
-          placeholder="Get a five-day forecast in your favorite cities"
-          className="form-control"
-          value={this.state.term}
-          onChange={this.onInputChange}
-        />
-        <span className="input-group-btn">
-          <button type="submit" className="btn btn-secondary"> Submit </button>
-        </span>
-      </form>
-    )
+    return (<form onSubmit={this.onFormSubmit} className="input-group">
+      <input placeholder="Get a five-day forecast in your favorite cities" className="form-control" value={this.state.term} onChange={this.onInputChange}/>
+      <span className="input-group-btn">
+        <button type="submit" className="btn btn-secondary">
+          Submit
+        </button>
+      </span>
+    </form>)
   }
 }
