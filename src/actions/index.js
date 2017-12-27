9f7800f2d4bde40370d2bb8a520aa8f05585651a
Daniel Mai <danielmai1728@gmail.com>
2017-12-26 21:53:14 -0800

    add fetch weather action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,12 @@
-const API_KEY = '016e914e169df06fa0d045287a239ef3'
+const API_KEY = '016e914e169df06fa0d045287a239ef3';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+
+export function fetchWeather(city) {
+  return {
+    type: FETCH_WEATHER
+  }
+
+}
