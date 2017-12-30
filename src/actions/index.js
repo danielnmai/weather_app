b83a54ab3a2c6abc3d314460bcdce42c3056b48b
Daniel Mai <danielmai1728@gmail.com>
2017-12-29 23:12:56 -0800

    add comment

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,12 +3,13 @@ import axios from 'axios';
 const API_KEY = '016e914e169df06fa0d045287a239ef3';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
+//fetch weather
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`;
   const request = axios.get(url);
-  
+
   return {
     type: FETCH_WEATHER,
     payload: request
