64c664120786b77f55193cd3a57fad347ea2b91a
Daniel Mai <danielmai1728@gmail.com>
2017-12-31 15:00:42 -0800

    fix CSS layout on table

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import _ from 'lodash';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
 
 export default (props) => {
   return (
     <div>
       <Sparklines height={120} width={180} data={props.data}>
         <SparklinesLine color={props.color} />
+        <SparklinesReferenceLine type='avg'/>
       </Sparklines>
     </div>
   )
