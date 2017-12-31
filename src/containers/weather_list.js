0bbae6ec8fd24e92f44d29354b2fb7822483eec5
Daniel Mai <danielmai1728@gmail.com>
2017-12-31 15:51:07 -0800

    converse K to F temps

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -4,30 +4,43 @@ import Chart from '../components/chart';
 // import './table.css';
 
 class WeatherList extends Component {
-  renderWeather(cityData, index, sth, arr){
+
+  onPost() {
+    // return (9/5 * (data - 273 ) + 32);
+    return "haha";
+  }
+
+  renderWeather(cityData){
+    function KelvinToFahrenheit(data) {
+      return (9/5 * (data - 273 ) + 32);
+    }
+
     const name = cityData.city.name;
-    const temps = cityData.list.map(weather => weather.main.temp);
+    const temps = cityData.list.map(weather => KelvinToFahrenheit(weather.main.temp));
+
+    // const temps = cityData.list.map(weather => weather.main.temp);
     const pressures = cityData.list.map(weather => weather.main.pressure);
     const humidities = cityData.list.map(weather => weather.main.humidity);
     return (
       <tr key={name}>
         <td>{name}</td>
-        <td><Chart data={temps} color="orange" /></td>
-        <td><Chart data={temps} color="green" /></td>
-        <td><Chart data={temps} color="blue" /></td>
+        <td><Chart data={temps} color="orange"  units="F"/></td>
+        <td><Chart data={pressures} color="green" units="hPa" /></td>
+        <td><Chart data={humidities} color="blue" units="%" /></td>
       </tr>
     )
   }
 
+
+
   render() {
-    console.log(this.props.weather);
     return (<table className="table table-hover">
       <thead>
         <tr>
           <th> City </th>
-          <th> Temperature (K)</th>
-          <th> Pressure (hPa) </th>
-          <th> Humidity (%) </th>
+          <th> Temperature </th>
+          <th> Pressure </th>
+          <th> Humidity </th>
         </tr>
       </thead>
       <tbody>
