9e8e4f18d9e5e6da9871f73087ef5555ea33bc88
Daniel Mai <danielmai1728@gmail.com>
2018-01-02 22:11:29 -0800

    remove all commas

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,18 +1,15 @@
-import axios from 'axios';
+import axios from 'axios'
 
-const API_KEY = '016e914e169df06fa0d045287a239ef3';
+const API_KEY = '016e914e169df06fa0d045287a239ef3'
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
 
 //fetch weather
-export const FETCH_WEATHER = 'FETCH_WEATHER';
+export const FETCH_WEATHER = 'FETCH_WEATHER'
 
 export function fetchWeather(city) {
-  const url = `${ROOT_URL}&q=${city},us`;
-  const request = axios.get(url);
-  console.log(url);
+  const url = `${ROOT_URL}&q=${city},us`
+  const request = axios.get(url)
+  console.log(url)
 
-  return {
-    type: FETCH_WEATHER,
-    payload: request
-  }
+  return {type: FETCH_WEATHER, payload: request}
 }
